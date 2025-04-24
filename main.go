628bd3eb7773a5b130b36c43b3a966171fbcb593
murilo nakaguma <murilo.nakaguma@aluno.senai.br>
2025-04-24 13:51:24 -0300

    exercicio media

--- main.go
+++ main.go
@@ -2,11 +2,19 @@ package main
 import (
 	"fmt"
 )
-func pegarNome()(string, string){ 
-  return "barry", "Allen"
+func analisarNota(nota1, nota2 float64)(float64, string){ 
+	media := (nota1 + nota2) / 2
+	var resultado string
+	if (media <= 6){
+		resultado = "reprovado"
+	}else {
+		resultado = "aprovado"
+	}
+	return media, resultado
 }
 func main(){
-nome, sobrenome := pegarNome()
-fmt.Println(nome, sobrenome)
+	
+media, resultado := analisarNota(7.5, 8.5)
+fmt.Println("media:", media, "resultado é:", resultado)
 }
 
